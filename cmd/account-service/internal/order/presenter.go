bbe6214884b6dfa053089a494de2fb47fa84ab8d
Nadezhda Frantseva <fnadezhda@fnadezhdaAMD6R.vmware.com>
2023-01-08 18:45:36 +0200

    Created env file

--- cmd/account-service/internal/order/presenter.go
+++ cmd/account-service/internal/order/presenter.go
@@ -2,7 +2,7 @@ package order
 
 import "github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/internal/repository/orderDB"
 
-type Repository interface {
+type Repository interface { // bridge - structural designa pattern
 	StoreOrder(order orderDB.Model) error
 	GetAllOrders() ([]orderDB.Model, error)
 }
--- cmd/prices-service/env/config.go
+++ cmd/prices-service/env/config.go
@@ -1,5 +1,11 @@
 package env
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 type WebSocetConfig struct {
 	CryptoURL    string
 	StockURL     string
@@ -9,13 +15,26 @@ type WebSocetConfig struct {
 	Secret       string
 }
 
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func LoadWebSocetConfig() WebSocetConfig {
+	key := goDotEnvVariable("KEY")
 	return WebSocetConfig{
 		CryptoURL:    "wss://stream.data.alpaca.markets/v1beta1/crypto",
 		StockURL:     "wss://stream.data.alpaca.markets/v2/iex",
 		CryptoQuotes: []string{"BTCUSD", "ETHUSD", "ADAUSD", "DOTUSD", "USDTUSD", "SOLUSD", "MATICUSD", "LINKUSD", "ATOMUSD", "BMBUSD", "LTCUSD"},
 		StockQuotes:  []string{"AAPL", "AMZN"},
-		Key:          "AK0NAAOFH7FREK1FKCXU",
+		Key:          key,
 		Secret:       "IYUve7og11abpgu4Qv8MIGCoFTd8HdALaLg6aaZ5",
 	}
 }
