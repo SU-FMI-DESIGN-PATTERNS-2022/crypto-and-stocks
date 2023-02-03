9287505b6a2f110d7bdb06529462eed1ececcec9
Nadezhda Frantseva <fnadezhda@vmware.com>
2023-02-03 14:54:13 +0200

    Make Order Endpoint

--- cmd/account-service/main.go
+++ cmd/account-service/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/env"
 	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/internal/order"
 	repository "github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/internal/repositories"
 	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/internal/repositories/order_repository"
 	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/account-service/internal/repositories/user_repository"
+	"github.com/gorilla/websocket"
+	"net/http"
 )
 
 func main() {
+
 	dbConfig := env.LoadDBConfig()
 	db, err := repository.Connect(dbConfig)
 
-	defer db.Close()
-
 	if err != nil {
-		panic(err)
+		fmt.Println("Failed to open database:", err)
+		return
 	}
+
+	defer db.Close()
 
 	orderRepository := order_repository.NewOrderTable(db)
 	userRepository := user_repository.NewUserTable(db)
 
-	order.NewPresenter(orderRepository, userRepository)
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	p := order.NewPresenter(orderRepository, userRepository, &upgrader)
+
+	http.HandleFunc("/order", p.StoreOrder)
+
+	fmt.Println("Starting server on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
