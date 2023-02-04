bdba675549ee0499a698b55efb4707e3c66fef33
Kristiyan Yanchev <kristiyan.yanchev@abv.bg>
2023-02-04 10:25:57 +0200

    Implemented websocket endpoint and update in mongo for crypto

--- cmd/prices-service/main.go
+++ cmd/prices-service/main.go
@@ -3,14 +3,45 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo"
-	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo/env"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo"
+	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo/env"
+	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo/prices_repository"
+	"github.com/asaskevich/EventBus"
+	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/cmd/prices-service/internal/stream"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func handler(w http.ResponseWriter, r *http.Request, bus EventBus.Bus) {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go writerCrypto(conn, bus)
+}
+
+func writerCrypto(conn *websocket.Conn, bus EventBus.Bus) {
+	bus.Subscribe("crypto", func(json interface{}) {
+		conn.WriteJSON(json)
+	})
+}
 
 func cryptoHandler(b []byte) {
 	var cryptoResponse []stream.CryptoResponse
@@ -19,6 +50,14 @@ func cryptoHandler(b []byte) {
 	}
 
 	fmt.Println(cryptoResponse)
+}
+
+func cryptoHandlerBus(b []byte, bus EventBus.Bus) {
+	var cryptoResponse []stream.CryptoResponse
+	if err := json.Unmarshal(b, &cryptoResponse); err != nil {
+		fmt.Println(err)
+	}
+	bus.Publish("crypto", cryptoResponse[len(cryptoResponse)-1])
 }
 
 func stockHandler(b []byte) {
@@ -31,10 +70,14 @@ func stockHandler(b []byte) {
 }
 
 func main() {
+	bus := EventBus.New()
+
 	mongoConfig := env.LoadMongoConfig()
 	ctx := context.TODO()
 	client, cancel, connectErr := mongo.Connect(mongoConfig)
-	//var pricesRepo = prices_repository.NewDatabase(client)
+
+	var pricesRepo = prices_repository.NewCollection(client, "crypto", "prices")
+
 	//var pricesPresenter = prices.NewPresenter(pricesRepo)
 
 	if connectErr != nil {
@@ -79,10 +122,31 @@ func main() {
 	}
 
 	go func() {
-		if err := cryptoStream.Start(cryptoHandler); err != nil {
+		if err := cryptoStream.Start(func(b []byte) { cryptoHandlerBus(b, bus) }); err != nil {
 			panic(err)
 		}
 	}()
+
+	bus.Subscribe("crypto", func(cryptoResponse stream.CryptoResponse) {
+
+		id := primitive.NewObjectID()
+		temp := prices_repository.Prices{
+			ID:       id,
+			Symbol:   cryptoResponse.Symbol,
+			Exchange: cryptoResponse.Exchange,
+			BidPrice: cryptoResponse.BidPrice,
+			AskPrice: cryptoResponse.AskPrice,
+			Date:     cryptoResponse.Date,
+		}
+
+		err := pricesRepo.StoreEntry(temp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) { handler(rw, r, bus) })
+	go log.Fatal(http.ListenAndServe(*addr, nil))
 
 	go func() {
 		if err := stockStream.Start(stockHandler); err != nil {
@@ -93,4 +157,5 @@ func main() {
 	time.Sleep(time.Minute)
 	cryptoStream.Stop()
 	stockStream.Stop()
+	print("ALL PRICES ________________________________________________________________________________________________________")
 }
