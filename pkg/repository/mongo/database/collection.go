e20cd652ecbf29fde0ecc8aa1640d7fb761dda78
Vasil <vortexblast550@gmail.com>
2023-07-04 00:57:20 +0300

    [mongo-tests] covered some of the functionalities related to mongo

---
+++ pkg/repository/mongo/database/collection.go
@@ -0,0 +1,4 @@
+package database
+
+var stocksCollection *Collection[StockPrices]
+var cryptoCollection *Collection[CryptoPrices]
--- pkg/repository/mongo/database/mongo_test.go
+++ pkg/repository/mongo/database/mongo_test.go
@@ -1,98 +1,151 @@
-package database_test
+package database
 
 import (
-	"github.com/SU-FMI-DESIGN-PATTERNS-2022/crypto-and-stocks/pkg/repository/mongo/env"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"os"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"testing"
+	"time"
 )
 
-var _ = Describe("Mongo Envs", func() {
-	const (
-		host     = "MONGO_HOST"
-		port     = "MONGO_PORT"
-		user     = "MONGO_USER"
-		database = "MONGO_DATABASE"
-		password = "MONGO_PASSWORD"
-		options  = "MONGO_OPTIONS"
-	)
-	const invalid = "invalid"
+// Create a new collection with the given name. The collection will be dropped when the test is over.
 
-	AfterEach(func() {
-		os.Unsetenv(host)
-		os.Unsetenv(port)
-		os.Unsetenv(user)
-		os.Unsetenv(database)
-		os.Unsetenv(password)
-		os.Unsetenv(options)
-	})
+func TestFindOne(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
 
-	When("invalid mongo host env is provided", func() {
-		BeforeEach(func() {
-			os.Setenv(host, invalid)
-		})
+	mt.Run("success", func(mt *mtest.T) {
+		//cryptoCollection = mt.Coll
+		//stockCollection = mt.Coll
 
-		It("should return an error", func() {
-			_, err := env.LoadMongoDBConfig()
-
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("failed to load mongo config"))
-		})
-	})
-
-	When("invalid mongo db name env is provided", func() {
-		BeforeEach(func() {
-			os.Setenv(database, invalid)
-		})
-
-		It("should return an error", func() {
-			_, err := env.LoadMongoDBConfig()
-
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("failed to load mongo config"))
-		})
-	})
-
-	When("no mongo url env is provided", func() {
-		It("should return the default mongo url and no error", func() {
-			mongoConfig, err := env.LoadMongoDBConfig()
-
-			Expect(err).ToNot(HaveOccurred())
-			Expect(mongoConfig.Host).To(Equal("mongodb://localhost:27017"))
-		})
-	})
-
-	When("no mongo db name env is provided", func() {
-		It("should return the default mongo db name and no error", func() {
-			mongoConfig, err := env.LoadMongoDBConfig()
-
-			Expect(err).ToNot(HaveOccurred())
-			Expect(mongoConfig.Database).To(Equal("crypto-and-stocks"))
-		})
-	})
-
-	When("mongo url env is provided", func() {
-		BeforeEach(func() {
-			os.Setenv(host, "mongodb://localhost:27018")
-		})
-
-		It("should return the value that is provided and no error", func() {
-			mongoConfig, err := env.LoadMongoDBConfig()
-
-			Expect(err).ToNot(HaveOccurred())
-			Expect(mongoConfig.Host).To(Equal("mongodb://localhost:27018"))
-		})
-	})
-
-	When("mongo db name env is provided", func() {
-		BeforeEach(func() {
-			os.Setenv(database, "crypto-and-stocks-test")
-		})
+		expectedStocks := StockPrices{
+			Prices: Prices{
+				Symbol:   "AAPL",
+				BidPrice: 100.0,
+				BidSize:  100.0,
+				AskPrice: 100.0,
+				AskSize:  100.0,
+				Date:     time.Now(),
+			},
+			AskExchange: "NASDAQ",
+			BidExchange: "NASDAQ",
+			TradeSize:   100.0,
+			Conditions:  []string{"A", "B"},
+			Tape:        "A",
+		}
+		expectedCryptos := CryptoPrices{
+			Prices: Prices{
+				Symbol:   "BTC",
+				BidPrice: 100.0,
+				BidSize:  100.0,
+				AskPrice: 100.0,
+				AskSize:  100.0,
+				Date:     time.Now(),
+			},
+			Exchange: "NASDAQ",
+		}
+		mt.AddMockResponses(mtest.CreateCursorResponse(1, "crypto", mtest.FirstBatch, bson.D{
+			{"_id", "1"},
+			{"symbol", expectedCryptos.Symbol},
+			{"bid_price", expectedCryptos.BidPrice},
+			{"bid_size", expectedCryptos.BidSize},
+			{"ask_price", expectedCryptos.AskPrice},
+			{"ask_size", expectedCryptos.AskSize},
+			{"date", expectedCryptos.Date},
+			{"exchange", expectedCryptos.Exchange},
+		}))
+		cryptoResponse, err := cryptoCollection.GetMostRecentPriceBySymbol("BTC")
+		assert.Nil(t, err)
+		assert.Equal(t, expectedCryptos, cryptoResponse)
+		mt.AddMockResponses(mtest.CreateCursorResponse(1, "stocks", mtest.FirstBatch, bson.D{
+			{"_id", "1"},
+			{"symbol", expectedStocks.Symbol},
+			{"bid_price", expectedStocks.BidPrice},
+			{"bid_size", expectedStocks.BidSize},
+			{"ask_price", expectedStocks.AskPrice},
+			{"ask_size", expectedStocks.AskSize},
+			{"date", expectedStocks.Date},
+			{"ask_exchange", expectedStocks.AskExchange},
+			{"bid_exchange", expectedStocks.BidExchange},
+			{"trade_size", expectedStocks.TradeSize},
+			{"conditions", expectedStocks.Conditions},
+			{"tape", expectedStocks.Tape},
+		}))
+		stockResponse, err := stocksCollection.GetMostRecentPriceBySymbol("AAPL")
+		assert.Nil(t, err)
+		assert.Equal(t, expectedStocks, stockResponse)
 
 	})
+}
 
-	When("mongo port env is provided", func() {
+func TestInsertOne(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
 
+	mt.Run("success", func(mt *mtest.T) {
+
+		mt.AddMockResponses(mtest.CreateSuccessResponse())
+
+		err := stocksCollection.StoreEntry(
+			StockPrices{
+				Prices: Prices{
+					Symbol:   "AAPL",
+					BidPrice: 100.0,
+					BidSize:  100.0,
+					AskPrice: 100.0,
+					AskSize:  100.0,
+					Date:     time.Now(),
+				},
+				AskExchange: "NASDAQ",
+				BidExchange: "NASDAQ",
+				TradeSize:   100.0,
+				Conditions:  []string{"A", "B"},
+				Tape:        "A",
+			})
+		assert.Nil(t, err)
 	})
+}
 
-})
+func TestInsertMany(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success", func(mt *mtest.T) {
+
+		mt.AddMockResponses(mtest.CreateSuccessResponse())
+
+		_, err := stocksCollection.insertMany("StockPrices", []interface{}{
+			StockPrices{
+				Prices: Prices{
+					Symbol:   "AAPL",
+					BidPrice: 100.0,
+					BidSize:  100.0,
+					AskPrice: 100.0,
+					AskSize:  100.0,
+					Date:     time.Now(),
+				},
+				AskExchange: "NASDAQ",
+				BidExchange: "NASDAQ",
+				TradeSize:   100.0,
+				Conditions:  []string{"A", "B"},
+				Tape:        "A",
+			},
+			StockPrices{
+				Prices: Prices{
+					Symbol:   "TSLA",
+					BidPrice: 100.0,
+					BidSize:  100.0,
+					AskPrice: 100.0,
+					AskSize:  100.0,
+					Date:     time.Now(),
+				},
+				AskExchange: "NASDAQ",
+				BidExchange: "NASDAQ",
+				TradeSize:   100.0,
+				Conditions:  []string{"A", "B"},
+				Tape:        "A",
+			},
+		})
+		assert.Nil(t, err)
+	})
+}
